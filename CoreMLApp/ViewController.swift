094a7e42ef0d4f75ac39d90b53f37c13ade54cb6
kenmaro3 <miha.ken.19@gmail.com>
2022-04-09 18:30:34 +0900

    Initial Commit

---
+++ CoreMLApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMLApp
+//
+//  Created by Kentaro Mihara on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
